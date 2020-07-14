91796bf30a32004e5bd974c051434dd9bea8369c
yo14 <ifliandry11@gmail.com>
2020-07-15 02:15:43 +0700

    Re-arrange template with base.html and make template for new scrape view

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/detik-popular')
 def detik_popular():
@@ -19,8 +19,13 @@ def detik_popular():
     titles = popular_area.find_all(attrs={'class': 'media__title'})
     images = popular_area.find_all(attrs={'class': 'media__image'})
 
-    return render_template('index.html', images=images)
+    return render_template('detik-scraper.html', images=images)
 
+@app.route('/idr-rates')
+def idr_rates():
+    source = requests.get('http://www.floatrates.com/daily/idr.json')
+    json_data = source.json()
+    return render_template('idr-rates.html', datas=json_data)
 
 
 if __name__=='__main__':
