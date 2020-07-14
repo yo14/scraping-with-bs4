ad1fe474ab7d1357922aba8d9167045400bc9042
yo14 <ifliandry11@gmail.com>
2020-07-14 15:48:26 +0700

    Collect data with bs4

--- detik_scraper.py
+++ detik_scraper.py
@@ -5,5 +5,15 @@ html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from':'
 
 soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-print(soup)
+popular_area = soup.find(attrs={'class':'grid-row list-content'})
+
+titles = popular_area.find_all(attrs={'class':'media__title'})
+images = popular_area.find_all(attrs={'class':'media__image'})
+
+
+for image in images:
+    print(image.find('a').find('img')['title'])
+
+
+# print(titles)
 
