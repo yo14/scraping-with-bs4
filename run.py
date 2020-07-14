93fcd12c1f3e9138526161858e49ee6b6f3da792
yo14 <ifliandry11@gmail.com>
2020-07-14 16:30:37 +0700

    Showing Data Scraping with Template

--- run.py
+++ run.py
@@ -8,7 +8,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/detik-popular')
+def detik_popular():
+    html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
 
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+    popular_area = soup.find(attrs={'class': 'grid-row list-content'})
+
+    titles = popular_area.find_all(attrs={'class': 'media__title'})
+    images = popular_area.find_all(attrs={'class': 'media__image'})
+
+    return render_template('index.html', images=images)
 
 
 
